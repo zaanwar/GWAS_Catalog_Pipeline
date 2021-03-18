531a6d75e30aacfe42cfb61eaeeb5eb7c49c560d
gsanramon <gsanramon@luc.edu>
2021-03-18 15:46:05 -0500

    trial add

---
+++ getFiles.py
@@ -0,0 +1,16 @@
+import os
+
+os.system()
+
+
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1
+
+
+
+
+
+
+
