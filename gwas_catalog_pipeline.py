eacbff2e73ddbee1f553bad8c0d2eae31b0cf117
Jessie Chen <jessiechen0513@gmail.com>
2021-04-16 16:42:57 -0500

    Update gwas_catalog_pipeline.py

--- gwas_catalog_pipeline.py
+++ gwas_catalog_pipeline.py
@@ -1,4 +1,7 @@
 import os
+#creating output file
+os.system("mkdir outputs")
+#running all scripts
 os.system("Rscript data.manip.R")
 os.system("Rscript get_gwas_associations.R")
 os.system("python3 Filtering_script.py")
