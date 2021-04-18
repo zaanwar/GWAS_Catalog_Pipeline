5c0e7aaec6a546a59ef7bd33b3cf7e5149142d48
Jessie Chen <jessiechen0513@gmail.com>
2021-04-18 08:29:37 -0500

    Update Filtering_script.py

--- Filtering_script.py
+++ Filtering_script.py
@@ -1,32 +1,31 @@
 #Script that filters related and unrelated results from the GWASRapid output
 
-# Dictionary that stores phenotype and EFO ID
-EFO_Dictionary = {'BMI' :'EFO_0008037', 
-'Chronic_kidney' : 'EFO_0003884',
-'C-reactive': 'EFO_000445',
-'End_renal' : 'EFO_000990',
-'Diastolic_blood' : 'EFO_0006336',
-'Systolic_blood' : 'EFO_0006335',
-'Smoking' : 'EFO_0004318',
-'Coffee ': 'EFO_0004330',
-'Triglyceride' : 'EFO_0004530', 
-'Total_cholesterol' : 'EFO_0004574', 
-'HDL_cholesterol' : 'EFO_0004612',
-'LDL_cholesterol' : 'EFO_0004195',
-'WBC' : 'EFO_0004308',
-'Waist-hip' : 'EFO_0004343',
-'Diabetes' : 'EFO_0000400',
-'Fasting_glucose' : 'EFO_0004468',
-'Fasting_insulin' : 'EFO_0004468',
-'Glomerular' : 'EFO_1002049',
-'Hemoglobin' : 'EFO_0009208',
-'Mean_corpuscular_hemoglobin' : 'EFO_0007629',
-'Hypertension' : 'EFO_0000537',
-'QRS_duration' : 'EFO_0005055',
-'QT_interval' : 'EFO_0004682',
-'PR_interval' : 'EFO_0004462',
-'Platelet' : 'EFO_0008446', 
-'Height' : 'EFO_0004339' }
+# List that stores ENSG gene name and associated phenotype from PrediXcan
+EFO_list = [
+['ENSG00000132693','EFO_000445'],
+['ENSG00000132693','EFO_0004308'],
+['ENSG00000160712', 'EFO_000445'],
+['ENSG00000130203','EFO_0004612'],
+['ENSG00000130203','EFO_0004574'],
+['ENSG00000130203','EFO_0004195'],
+['ENSG00000130203','EFO_000445'],
+['ENSG00000135218','EFO_0004612'],
+['ENSG00000079739', 'EFO_0004574'],
+['ENSG00000079739', 'EFO_0004530'],
+['ENSG00000158710', 'EFO_0004308'],
+['ENSG00000073754', 'EFO_0004308'],
+['ENSG00000132703', 'EFO_000445'],
+['ENSG00000136689', 'EFO_000445'],
+['ENSG00000257017', 'EFO_0004195'],
+['ENSG00000257017', 'EFO_0004574'],
+['ENSG00000066294', 'EFO_0004308'],
+['ENSG00000143590', 'EFO_000445'],
+['ENSG00000143556', 'EFO_000445'],
+['ENSG00000118137', 'EFO_0004530'],
+['ENSG00000243364', 'EFO_0004308'],
+['ENSG00000122224', 'EFO_0004308'],
+['ENSG00000160856','EFO_0004308']]
+
 
 
 #open test file
@@ -39,23 +38,28 @@ related_output = open('outputs/output_related.txt','w')
 
 unrelated_output = open('outputs/output_unrelated.txt','w')
 
+#writes header column names from snptrait table to the output files
+firstLine = open_file.pop(0)
+related_output.write(firstLine +' \n')
+unrelated_output.write(firstLine + '\n')
 
-
-#iterate through given phenotypes and return EFO value for the given phenotype
+#iterate through given SNP associations and categorizes as related/unrelated based on given list of gene/phenotype pairs
 for line in open_file:
     #print(line)
     split_line = line.split(',') #split string
+    ENSG_gene = str(split_line[2])
+    ENSG_gene = ENSG_gene[1:-1]
     EFO_ID  = str(split_line[-1])
-    #print(EFO_ID)
     if EFO_ID[0] == '\"':#removes quotation from the beginning of the EFO ID
         EFO_ID = EFO_ID[1:]
     if EFO_ID[-1] == '\"':#removes quotation from the end of the EFO ID
         EFO_ID = EFO_ID[:-1]
-    contains_EFO_ID = EFO_ID in EFO_Dictionary.values()
-    if contains_EFO_ID == True: #checks to see if EFO_ID is in the dictionary
-        related_output.write(line +' \n')#if it is in the dictionary it is a related output
+    gene_trait = [ENSG_gene, EFO_ID]
+    contains_pair = gene_trait in EFO_list
+    if contains_pair == True: #checks to see if pairing is in the list
+        related_output.write(line +' \n')#if it is in the list it is a related output
     else:
-        unrelated_output.write(line + '\n')#if it is not in the dictionary it is an unrelated output
+        unrelated_output.write(line + '\n')#if it is not in the list it is an unrelated output
         
 related_output.close()
 unrelated_output.close()
