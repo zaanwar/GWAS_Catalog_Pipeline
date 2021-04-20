3dcc4398aa170b6b0fb17a2ff362980c9f4ab8e2
Jessie Chen <jessiechen0513@gmail.com>
2021-04-20 16:28:04 -0500

    Update data.manip.R

--- data.manip.R
+++ data.manip.R
@@ -5,7 +5,7 @@
 #raw.file.path <- paste(wk.dir, "/", "GWAS_Catalog_Pipeline/group8", "/", sep = "")  
   
 #raw.file <- read.csv(paste(p.data.raw, rawSPrediScan.filename, sep = ""), stringsAsFactors = FALSE)
-raw.file <- read.csv("inputs/example_SPrediScan_output.csv", stringsAsFactors = FALSE)
+raw.file <- read.csv("inputs/full_run/example_SPrediScan_output.csv", stringsAsFactors = FALSE)
 
 library(purrr) #to use map
 GENE <- as.character(map(strsplit(raw.file$GENE, split="_"),2))
