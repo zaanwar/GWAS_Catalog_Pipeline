ecb5c1382b7556a1912c0857636112c91c8a3ed2
Jessie Chen <jessiechen0513@gmail.com>
2021-04-20 16:15:17 -0500

    Update data.manip.R

--- data.manip.R
+++ data.manip.R
@@ -5,7 +5,7 @@
 #raw.file.path <- paste(wk.dir, "/", "GWAS_Catalog_Pipeline/group8", "/", sep = "")  
   
 #raw.file <- read.csv(paste(p.data.raw, rawSPrediScan.filename, sep = ""), stringsAsFactors = FALSE)
-raw.file <- read.csv("group8/example_SPrediScan_output.csv", stringsAsFactors = FALSE)
+raw.file <- read.csv("inputs/example_SPrediScan_output.csv", stringsAsFactors = FALSE)
 
 library(purrr) #to use map
 GENE <- as.character(map(strsplit(raw.file$GENE, split="_"),2))
@@ -13,7 +13,7 @@ Phenotype <- raw.file$Phenotype
 
 ensg.pheno <- cbind(GENE,Phenotype)
 #write.csv(ensg.pheno, paste(raw.file.path, "clean_SPrediScan_ID-pheno.csv", sep = ""), row.names = FALSE)
-write.csv(ensg.pheno, "group8/clean_SPrediScan_ID-pheno.csv", row.names = FALSE)
+write.csv(ensg.pheno, "inputs/clean_SPrediScan_ID-pheno.csv", row.names = FALSE)
 
 #get unique GENE & phenotype separately
 unique(GENE) #17 unique genes
@@ -23,4 +23,4 @@ unique(Phenotype) #6 unique phenotypes
 uniq.combo <- unique(ensg.pheno)
 #write in separate file
 #write.csv(uniq.combo, paste(raw.file.path, "unique_ID-pheno.csv", sep = ""), row.names = FALSE)
-write.csv(uniq.combo, "group8/unique_ID-pheno.csv", row.names = FALSE)
+write.csv(uniq.combo, "inputs/unique_ID-pheno.csv", row.names = FALSE)
